8a69287270fc0f7799dfb2c421d5fcdf84ab1a03
emperorlu <emperorlu@hust.edu.cn>
2021-04-12 17:58:16 +0800

    add blog

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -50,6 +50,25 @@ $(document).ready(function(){
     stickySideBar();
   });
 
+  // Back to top
+
+  $("#back-top").hide();
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
+    $('#back-top a').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+  });
+
   // Follow menu drop down
 
   $(".author__urls-wrapper button").on("click", function() {
